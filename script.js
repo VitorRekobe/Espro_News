c11752695857906f391eaa9d2df631f997e8b1ae
Vitor Gabriel S. Garcia <98287250+VitorRekobe@users.noreply.github.com>
2023-08-13 20:53:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-// function Hamburgue() {
-//     var divHamburgue = document.getElementById("DivHamburgue");
-//     if (divHamburgue.style.display === "block") {
-//         divHamburgue.style.display = "none";
-//     } else {
-//         divHamburgue.style.display = "block";
-//     }
-// }
-
 function Hamburgue() {
     var divHamburgue = document.getElementById("DivHamburgue");
     divHamburgue.classList.toggle('active');
